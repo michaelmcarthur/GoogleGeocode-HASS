8fb42adc2409ed31276f191121ecbc4c92f9ec71
michaelmcarthur <michaelmcarthur@gmail.com>
2017-07-29 11:50:09 +0100

    Added hide zone option

--- google_geocode.py
+++ google_geocode.py
@@ -22,6 +22,7 @@ import homeassistant.helpers.config_validation as cv
 
 CONF_ORIGIN = 'origin'
 CONF_OPTIONS = 'options'
+CONF_DISPLAY_ZONE = 'display_zone'
 CONF_ATTRIBUTION = "Data provided by maps.google.com"
 
 ATTR_STREET_NUMBER = 'Street Number'
@@ -31,10 +32,12 @@ ATTR_POSTAL_TOWN = 'Postal Town'
 ATTR_POSTAL_CODE = 'Postal Code'
 ATTR_REGION = 'State'
 ATTR_COUNTRY = 'Country'
+ATTR_COUNTY = 'County'
 ATTR_FORMATTED_ADDRESS = 'Formatted Address'
 
 DEFAULT_NAME = 'Google Geocode'
-DEFAULT_OPTION = 'street'
+DEFAULT_OPTION = 'both'
+DEFAULT_DISPLAY_ZONE = 'display'
 current = '0,0'
 zone_check = 'a'
 SCAN_INTERVAL = timedelta(seconds=60)
@@ -42,6 +45,7 @@ SCAN_INTERVAL = timedelta(seconds=60)
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_ORIGIN): cv.string,
     vol.Optional(CONF_OPTIONS, default=DEFAULT_OPTION): cv.string,
+    vol.Optional(CONF_DISPLAY_ZONE, default=DEFAULT_DISPLAY_ZONE): cv.string,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     vol.Optional(CONF_SCAN_INTERVAL, default=SCAN_INTERVAL):
         cv.time_period,
@@ -54,18 +58,20 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     name = config.get(CONF_NAME)
     origin = config.get(CONF_ORIGIN)
     options = config.get(CONF_OPTIONS)
+    display_zone = config.get(CONF_DISPLAY_ZONE)
 
-    add_devices([GoogleGeocode(hass, origin, name, options)])
+    add_devices([GoogleGeocode(hass, origin, name, options, display_zone)])
     
 
 class GoogleGeocode(Entity):
     """Representation of a Google Geocode Sensor."""
 
-    def __init__(self, hass, origin, name, options):
+    def __init__(self, hass, origin, name, options, display_zone):
         """Initialize the sensor."""
         self._hass = hass
         self._name = name
         self._options = options.lower()
+        self._display_zone = display_zone.lower()
         self._state = "Awaiting Update"
         
         self._street_number = None
@@ -109,7 +115,7 @@ class GoogleGeocode(Entity):
             ATTR_POSTAL_CODE: self._postal_code,
             ATTR_REGION: self._region,
             ATTR_COUNTRY: self._country,
-            ATTR_COUNTRY: self._county,
+            ATTR_COUNTY: self._county,
             ATTR_ATTRIBUTION: CONF_ATTRIBUTION,
             ATTR_FORMATTED_ADDRESS: self._formatted_address,
         }
@@ -129,63 +135,65 @@ class GoogleGeocode(Entity):
         global zone_check
         zone_check = self.hass.states.get(self._origin_entity_id).state
         
-        if zone_check == 'not_home':
-            if current == self._origin:
-                pass
-            elif self._origin == None:
-                pass
-            else:
-                lat = self._origin
-                current = lat
-                self._reset_attributes()
-                url = "https://maps.google.com/maps/api/geocode/json?latlng=" + lat
-                response = get(url)
-                json_input = response.text
-                decoded = json.loads(json_input)
-                street_number = ''
-                street = 'Unnamed Road'
-                alt_street = 'Unnamed Road'
-                city = ''
-                postal_town = ''
-                formatted_address = ''
-                state = ''
-                county = ''
-                country = ''
-                
-                for result in decoded["results"]:
-                    for component in result["address_components"]:
-                      if 'street_number' in component["types"]:
-                          street_number = component["long_name"]
-                          self._street_number = street_number
-                      if 'route' in component["types"]:
-                          street = component["long_name"]
-                          self._street = street
-                      if 'sublocality_level_1' in component["types"]:
-                          alt_street = component["long_name"]
-                      if 'postal_town' in component["types"]:
-                          postal_town = component["long_name"]
-                          self._postal_town = postal_town
-                      if 'locality' in component["types"]:
-                          city = component["long_name"]
-                          self._city = city
-                      if 'administrative_area_level_1' in component["types"]:
-                          state = component["long_name"]
-                          self._region = state
-                      if 'administrative_area_level_2' in component["types"]:
-                          county = component["long_name"]
-                          self._county = county
-                      if 'country' in component["types"]:
-                          country = component["long_name"]
-                          self._country = country
-                      if 'postal_code' in component["types"]:
-                          postal_code = component["long_name"]
-                          self._postal_code = postal_code
-                if 'formatted_address' in decoded['results'][0]:
-                    formatted_address = decoded['results'][0]['formatted_address']
-                    self._formatted_address = formatted_address
+        
+        if current == self._origin:
+            pass
+        elif self._origin == None:
+            pass
+        else:
+            lat = self._origin
+            current = lat
+            self._reset_attributes()
+            url = "https://maps.google.com/maps/api/geocode/json?latlng=" + lat
+            response = get(url)
+            json_input = response.text
+            decoded = json.loads(json_input)
+            street_number = ''
+            street = 'Unnamed Road'
+            alt_street = 'Unnamed Road'
+            city = ''
+            postal_town = ''
+            formatted_address = ''
+            state = ''
+            county = ''
+            country = ''
+            
+            for result in decoded["results"]:
+                for component in result["address_components"]:
+                  if 'street_number' in component["types"]:
+                      street_number = component["long_name"]
+                      self._street_number = street_number
+                  if 'route' in component["types"]:
+                      street = component["long_name"]
+                      self._street = street
+                  if 'sublocality_level_1' in component["types"]:
+                      alt_street = component["long_name"]
+                  if 'postal_town' in component["types"]:
+                      postal_town = component["long_name"]
+                      self._postal_town = postal_town
+                  if 'locality' in component["types"]:
+                      city = component["long_name"]
+                      self._city = city
+                  if 'administrative_area_level_1' in component["types"]:
+                      state = component["long_name"]
+                      self._region = state
+                  if 'administrative_area_level_2' in component["types"]:
+                      county = component["long_name"]
+                      self._county = county
+                  if 'country' in component["types"]:
+                      country = component["long_name"]
+                      self._country = country
+                  if 'postal_code' in component["types"]:
+                      postal_code = component["long_name"]
+                      self._postal_code = postal_code
+            if 'formatted_address' in decoded['results'][0]:
+                formatted_address = decoded['results'][0]['formatted_address']
+                self._formatted_address = formatted_address
                     
+            if self._display_zone == 'hide':
                 if street == 'Unnamed Road':
                     street = alt_street
+                    self._street = alt_street
                 if city == '':
                     city = postal_town
                     if city == '':
@@ -205,10 +213,9 @@ class GoogleGeocode(Entity):
                     ADDRESS = country
                     
                 self._state = ADDRESS
-                
-        else:
-            self._state = zone_check[0].upper() + zone_check[1:]
-            self._reset_attributes()
+            else:
+                self._state = zone_check[0].upper() + zone_check[1:]
+                # self._reset_attributes()
 
 
     def _get_location_from_entity(self, entity_id):
